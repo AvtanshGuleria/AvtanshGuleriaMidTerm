a3300d4918de39ab32116d278fd2994de50e9219
guleravt <guleravt@sheridancollege.ca>
2024-02-21 23:29:20 +0530

    Input Componment Implemented

--- MidTermAvtanshGuleria/src/app/input/input.component.ts
+++ MidTermAvtanshGuleria/src/app/input/input.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,FormsModule,ReactiveFormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  outputData(data:any){
+    console.log(data);
+    
+  }
 }
